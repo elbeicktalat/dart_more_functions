bcae937a98240e3ea3b94b205cf7f7ff90e19acd
elbeicktalat <talatelbeick@gmail.com>
2021-11-27 19:32:47 +0100

    rename string assertion functions

--- lib/src/string/cases/assertions/extension_string_case_assertions.dart
+++ lib/src/string/cases/assertions/extension_string_case_assertions.dart
@@ -7,7 +7,7 @@ extension ExtensionsStringCaseAssertion on String {
   /// 'DARt'.isUpperCase // false
   /// 'DART'.isUpperCase // true
   ///```
-  bool get isUpperCase => funIsUpperCase(this);
+  bool get isUpperCase => getIsUpperCase(this);
 
   ///[isLowerCase] check if the entire characters is minuscule.
   ///```dart
@@ -15,7 +15,7 @@ extension ExtensionsStringCaseAssertion on String {
   /// 'darT'.isLowerCase // false
   /// 'dart'.isLowerCase // true
   ///```
-  bool get isLowerCase => funIsLowerCase(this);
+  bool get isLowerCase => getIsLowerCase(this);
 
   ///[firstIsUpperCase] check if the first character is majuscule.
   ///```dart
@@ -23,7 +23,7 @@ extension ExtensionsStringCaseAssertion on String {
   /// 'dART'.firstIsUpperCase // false
   /// 'Dart'.firstIsUpperCase // true
   ///```
-  bool get firstIsUpperCase => funFirstIsUpperCase(this);
+  bool get firstIsUpperCase => getFirstIsUpperCase(this);
 
   ///[lastIsUpperCase] check if the last character is majuscule.
   ///```dart
@@ -31,7 +31,7 @@ extension ExtensionsStringCaseAssertion on String {
   /// 'DARt'.lastIsUpperCase // false
   /// 'darT'.lastIsUpperCase // true
   ///```
-  bool get lastIsUpperCase => funLastIsUpperCase(this);
+  bool get lastIsUpperCase => getLastIsUpperCase(this);
 
   ///[firstIsLowerCase] check if the first character is minuscule.
   ///```dart
@@ -39,7 +39,7 @@ extension ExtensionsStringCaseAssertion on String {
   /// 'Dart'.firstIsLowerCase // false
   /// 'darT'.firstIsLowerCase // true
   ///```
-  bool get firstIsLowerCase => funFirstIsLowerCase(this);
+  bool get firstIsLowerCase => getFirstIsLowerCase(this);
 
   ///[lastIsLowerCase] check if the last character is minuscule.
   ///```dart
@@ -47,19 +47,19 @@ extension ExtensionsStringCaseAssertion on String {
   /// darT'.lastIsLowerCase // false
   /// Dart'.lastIsLowerCase // true
   ///```
-  bool get lastIsLowerCase => funLastIsLowerCase(this);
+  bool get lastIsLowerCase => getLastIsLowerCase(this);
 
   ///[charAtIsUpperCase()] check if character at [index] is majuscule.
   ///```dart
   /// 'Dart'.charAtIsUpperCase(1) // false
   /// 'dArt'.charAtIsUpperCase(1) // true
   ///```
-  bool charAtIsUpperCase(int index) => funCharAtIsUpperCase(this, index);
+  bool charAtIsUpperCase(int index) => getCharAtIsUpperCase(this, index);
 
   ///[charAtIsLowerCase()] check if character at [index] is minuscule.
   ///```dart
   /// 'dArt'.charAtIsLowerCase(1) // false
   /// 'DaRT'.charAtIsLowerCase(1) // true
   ///```
-  bool charAtIsLowerCase(int index) => funCharAtIsLowerCase(this, index);
+  bool charAtIsLowerCase(int index) => getCharAtIsLowerCase(this, index);
 }
--- lib/src/string/cases/assertions/string_case_assertions.dart
+++ lib/src/string/cases/assertions/string_case_assertions.dart
@@ -1,79 +1,79 @@
 part of string_helpers;
 
-///[getIsUpperCase()] check if the entire characters of the given [string] is uppercase.
+///[getIsUpperCase] check if the entire characters of the given [string] is uppercase.
 ///```dart
 /// isUpper('Dart') // false
 /// isUpper('DARt') // false
 /// isUpper('DART') // true
 ///```
-bool funIsUpperCase(String string) {
+bool getIsUpperCase(String string) {
   return string == string.toUpperCase();
 }
 
-///[getIsLowerCase()] check if the entire characters of the given [string] is lowercase.
+///[getIsLowerCase] check if the entire characters of the given [string] is lowercase.
 ///```dart
 /// isLower('Dart') // false
 /// isLower('darT') // false
 /// isLower('dart') // true
 ///```
-bool funIsLowerCase(String string) {
+bool getIsLowerCase(String string) {
   return string == string.toLowerCase();
 }
 
-///[getFirstIsUpperCase()] check if the first character of the given [string] is uppercase.
+///[getFirstIsUpperCase] check if the first character of the given [string] is uppercase.
 ///```dart
 /// firstIsUpper('dart') // false
 /// firstIsUpper('dART') // false
 /// firstIsUpper('Dart') // true
 ///```
-bool funFirstIsUpperCase(String string) {
+bool getFirstIsUpperCase(String string) {
   return firstChar(string) == firstChar(string).toUpperCase();
 }
 
-///[getLastIsUpperCase()] check if the last character of the given [string] is uppercase.
+///[getLastIsUpperCase] check if the last character of the given [string] is uppercase.
 ///```dart
 /// lastIsUpper('dart') // false
 /// lastIsUpper('DARt') // false
 /// lastIsUpper('darT') // true
 ///```
-bool funLastIsUpperCase(String string) {
+bool getLastIsUpperCase(String string) {
   return lastChar(string) == lastChar(string).toUpperCase();
 }
 
-///[getFirstIsLowerCase()] check if the first character of the given [string] is lowercase.
+///[getFirstIsLowerCase] check if the first character of the given [string] is lowercase.
 ///```dart
 /// firstIsLower('DART') // false
 /// firstIsLower('Dart') // false
 /// firstIsLower('darT') // true
 ///```
-bool funFirstIsLowerCase(String string) {
+bool getFirstIsLowerCase(String string) {
   return firstChar(string) == firstChar(string).toLowerCase();
 }
 
-///[getLastIsLowerCase()] check if the last character of the given [string] is lowercase.
+///[getLastIsLowerCase] check if the last character of the given [string] is lowercase.
 ///```dart
 /// lastIsLower('DART') // false
 /// lastIsLower('darT') // false
 /// lastIsLower('Dart') // true
 ///```
-bool funLastIsLowerCase(String string) {
+bool getLastIsLowerCase(String string) {
   return lastChar(string) == lastChar(string).toLowerCase();
 }
 
-///[getCharAtIsUpperCase()] check if character at that specific [index] of the given [string] is uppercase.
+///[getCharAtIsUpperCase] check if character at that specific [index] of the given [string] is uppercase.
 ///```dart
 /// charAtIsUpper('Dart', 1) // false
 /// charAtIsUpper('dART', 1) // true
 ///```
-bool funCharAtIsUpperCase(String string, int index) {
+bool getCharAtIsUpperCase(String string, int index) {
   return charAt(string, index) == charAt(string, index).toUpperCase();
 }
 
-///[charAtIsLower()] check if character at that specific [index] of the given [string] is lowercase.
+///[getCharAtIsLowerCase] check if character at that specific [index] of the given [string] is lowercase.
 ///```dart
 /// charAtIsUpper('Dart', 1) // true
 /// charAtIsUpper('dART', 1) // false
 ///```
-bool funCharAtIsLowerCase(String string, int index) {
+bool getCharAtIsLowerCase(String string, int index) {
   return charAt(string, index) == charAt(string, index).toLowerCase();
 }
