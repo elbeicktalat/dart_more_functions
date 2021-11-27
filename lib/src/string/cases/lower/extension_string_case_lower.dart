371eacc90d60cada62d21cfaf6d8147fd5967b8d
elbeicktalat <talatelbeick@gmail.com>
2021-11-27 19:10:38 +0100

    add extensions functions for string case lower methods.

---
+++ lib/src/string/cases/lower/extension_string_case_lower.dart
@@ -0,0 +1,38 @@
+part of string_helpers;
+
+extension ExtensionsStringCaseLower on String {
+  ///Converts first character in this string to lower case.
+  ///
+  /// If the first character is already in lower case, this function returns `this`.
+  /// ```dart
+  /// 'Dart'.firstToLowerCase; // 'dart'
+  /// 'abc'.firstToLowerCase; // 'abc'
+  /// ```
+  String get firstToLowerCase => getFirstToLowerCase(this);
+
+  ///Converts last character in this string to lower case.
+  ///
+  /// If the last character is already in lower case, this function returns `this`.
+  /// ```dart
+  /// 'Dart'.firstToLowerCase; // 'dart'
+  /// 'abc'.firstToLowerCase; // 'abc'
+  /// ```
+  String get lastToLowerCase => getLastToLowerCase(this);
+
+  ///Converts first character in this string to lower case, and first character of next word to upper case.
+  ///
+  ///```dart
+  /// 'LOWER CAMEL CASE'.toLowerCamelCase; // 'lower Camel Case'
+  ///```
+  String get toLowerCamelCase => getLowerCamelCase(this);
+
+  /// **toLowerSnakeCase** known as **toLowerUnderscoreCase**.
+  ///
+  ///Converts all characters in this string to lower case, then replace white space with underscore `_`.
+  ///
+  ///```dart
+  /// 'lower underscore case'.toLowerSnakeCase; // 'lower_underscore_case'
+  /// 'LOWER UNDERSCORE CASE'.toLowerSnakeCase; // 'lower_underscore_case'
+  ///```
+  String get toLowerSnakeCase => getLowerSnakeCase(this);
+}
--- lib/src/string/string_helpers.dart
+++ lib/src/string/string_helpers.dart
@@ -4,6 +4,10 @@ part 'cases/assertions/extension_string_case_assertions.dart';
 
 part 'cases/assertions/string_case_assertions.dart';
 
+part 'cases/lower/extension_string_case_lower.dart';
+
+part 'cases/lower/string_case_lower.dart';
+
 part 'cases/upper/extension_string_case_upper.dart';
 
 part 'cases/upper/string_case_upper.dart';
