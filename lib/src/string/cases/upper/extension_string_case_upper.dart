063f94ae6faa25f5859f4dbcc505335a546d6c0e
elbeicktalat <talatelbeick@gmail.com>
2021-11-27 19:01:34 +0100

    add extensions functions for string case upper methods.

---
+++ lib/src/string/cases/upper/extension_string_case_upper.dart
@@ -0,0 +1,38 @@
+part of string_helpers;
+
+extension ExtensionsStringCaseUpper on String {
+  ///Converts first character in this string to upper case.
+  ///
+  /// If the first character is already in upper case, this function returns `this`.
+  /// ```dart
+  /// 'dart'.firstToUpperCase; // 'Dart'
+  /// 'abc'.firstToUpperCase; // 'Abc'
+  /// ```
+  String get firstToUpperCase => getFirstToUpperCase(this);
+
+  ///Converts last character in this string to upper case.
+  ///
+  /// If the last character is already in upper case, this function returns `this`.
+  /// ```dart
+  /// 'dart'.firstToLowerCase; // 'darT'
+  /// 'abc'.firstToLowerCase; // 'abC'
+  /// ```
+  String get lastToUpperCase => getLastToUpperCase(this);
+
+  ///Converts first character in this string to upper case, and first character of next word to upper case.
+  ///
+  ///```dart
+  /// 'upper camel case'.toLowerCamelCase; // 'Upper Camel Case'
+  ///```
+  String get toUpperCamelCase => getUpperCamelCase(this);
+
+  /// **toUpperSnakeCase** known as **toUpperUnderscoreCase**.
+  ///
+  ///Converts all characters in this string to upper case, then replace white space with underscore `_`.
+  ///
+  ///```dart
+  /// 'upper underscore case'.toLowerSnakeCase; // 'UPPER_UNDERSCORE_CASE'
+  /// 'LOWER UNDERSCORE CASE'.toLowerSnakeCase; // 'UPPER_UNDERSCORE_CASE'
+  ///```
+  String get toUpperSnakeCase => getUpperSnakeCase(this);
+}
--- lib/src/string/string_helpers.dart
+++ lib/src/string/string_helpers.dart
@@ -4,8 +4,8 @@ part 'cases/assertions/extension_string_case_assertions.dart';
 
 part 'cases/assertions/string_case_assertions.dart';
 
-part 'cases/string_case_lower.dart';
+part 'cases/upper/extension_string_case_upper.dart';
 
-part 'cases/string_case_upper.dart';
+part 'cases/upper/string_case_upper.dart';
 
 part 'chars/char.dart';
