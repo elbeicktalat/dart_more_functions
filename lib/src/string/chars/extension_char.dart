31d4af7afd43a0d8d8800777c0c9cf3a671f482b
elbeicktalat <talatelbeick@gmail.com>
2021-11-27 20:28:24 +0100

    add extensions functions for char methods.

---
+++ lib/src/string/chars/extension_char.dart
@@ -0,0 +1,31 @@
+part of string_helpers;
+
+extension ExtensionChar on String {
+  ///[firstChar] returns the first character in this string,
+  /// without leading and trailing whitespace.
+  /// ```dart
+  /// 'dart'.firstChar; // returns 'd'
+  /// '  dart'.firstChar; // returns 'd'
+  /// ```
+  String get firstChar => getFirstChar(this);
+
+  ///[lastChar] returns the last character in this string,
+  /// without leading and trailing whitespace.
+  /// ```dart
+  /// 'dart'.lastChar; // returns 't'
+  /// 'dart  '.lastChar; // returns 't'
+  /// ```
+  String get lastChar => getLastChar(this);
+
+  ///[lastCharIndex] returns the index of the last character in this string,
+  /// without leading and trailing whitespace counting.
+  int get lastCharIndex => getLastCharIndex(this);
+
+  ///[charAt] returns character in this string at that [index],
+  /// without leading and trailing whitespace.
+  /// ```dart
+  /// charAt('dart', 1) // returns 'a'
+  /// charAt('  dart   ', 1) // returns 'a'
+  /// ```
+  String charAt(int index) => getCharAt(this, index);
+}
--- lib/src/string/string_helpers.dart
+++ lib/src/string/string_helpers.dart
@@ -1,15 +1,11 @@
 library string_helpers;
 
 part 'cases/assertions/extension_string_case_assertions.dart';
-
 part 'cases/assertions/string_case_assertions.dart';
-
 part 'cases/lower/extension_string_case_lower.dart';
-
 part 'cases/lower/string_case_lower.dart';
-
 part 'cases/upper/extension_string_case_upper.dart';
-
 part 'cases/upper/string_case_upper.dart';
-
 part 'chars/char.dart';
+
+part 'chars/extension_char.dart';
