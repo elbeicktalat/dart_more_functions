47b7e636710eeafb2c7ba8248dd3009637f99051
elbeicktalat <talatelbeick@gmail.com>
2021-11-30 11:59:32 +0100

    add add_whitespace

---
+++ lib/src/string/utils/whitespace/add_whitespace.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2021. Talat El Beick. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+part of string_helpers;
+
+extension StringAddWhitespace on String {
+//add or insert
+//TODO: add insert whitespace after [char].
+//TODO: add insert whitespace before [char].
+//TODO: add insert whitespace after [word].
+//TODO: add insert whitespace before [word].
+}
