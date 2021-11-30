532f79de58d94a897a0362aa96abcd61908c7bc7
elbeicktalat <talatelbeick@gmail.com>
2021-11-30 15:02:21 +0100

    add some symbol assertion

--- lib/src/string/utils/symbol/symbol_assertion.dart
+++ lib/src/string/utils/symbol/symbol_assertion.dart
@@ -5,6 +5,24 @@
 part of string_helpers;
 
 extension StringSymbolAssertion on String {
+  ///Check if this [string] has symbols.
+  ///```dart
+  /// 'Hello world'.hasSymbols; // returns false
+  /// 'hello world!'.hasSymbols; // returns true
+  ///```
+  bool get hasSymbols {
+    return contains(RegExp(r'[^\w\s]+'));
+  }
+
+  ///Check if this [string] has only symbols.
+  ///```dart
+  /// 'Hello world!'.hasOnlySymbols; // returns false
+  /// '@ # % !'.hasOnlySymbols; // returns true
+  ///```
+  bool get hasOnlySymbols {
+    return RegExp(r'[\w\s]+ ').allMatches(this).isEmpty;
+  }
+
 //assertions
 //TODO: add is symbol.
 //TODO: add has symbol before [char].
