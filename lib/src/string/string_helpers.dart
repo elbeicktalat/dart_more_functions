44093030e73fcf198b1ed7aadcb9f9ea793540ce
elbeicktalat <talatelbeick@gmail.com>
2021-11-30 11:43:14 +0100

    add char transformation

--- lib/src/string/string_helpers.dart
+++ lib/src/string/string_helpers.dart
@@ -5,25 +5,17 @@
 library string_helpers;
 
 part 'case/string_case_assertion.dart';
-
 part 'case/string_lower_case.dart';
-
 part 'case/string_upper_case.dart';
-
 part 'char/char_getter.dart';
-
 part 'char/char_position.dart';
 
+part 'char/char_transformation.dart';
+
 part 'utils/counters.dart';
-
 part 'utils/symbol/add_symbol.dart';
-
 part 'utils/symbol/remove_symbol.dart';
-
 part 'utils/symbol/symbol_assertion.dart';
-
 part 'utils/whitespace/add_whitespace.dart';
-
 part 'utils/whitespace/remove_whitespace.dart';
-
 part 'utils/whitespace/whitespace_assertion.dart';
